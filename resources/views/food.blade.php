ba617cc74885211186c9dada18c478d5446ccb53
Nguyễn Đỗ Thế Nguyên <thenguyen1024@gmail.com>
2022-07-09 09:06:34 +0700

    food form

--- resources/views/food.blade.php
+++ resources/views/food.blade.php
@@ -9,25 +9,31 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1"
-                                   aria-describedby="emailHelp" placeholder="Enter email">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                                else.</small>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div>
+                            <x-label for="name" :value="__('Name')"/>
+
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+                                     required autofocus/>
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1"
-                                   placeholder="Password">
+
+                        <div class="mt-4">
+                            <x-label for="price" :value="__('Price')"/>
+
+                            <x-input id="price" class="block mt-1 w-full" type="number" name="price"
+                                     :value="old('price')" required autofocus/>
                         </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+                        <div class="flex items-center justify-start mt-4">
+                            <x-button class="ml-3">
+                                {{ __('Log in') }}
+                            </x-button>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
+
                 </div>
             </div>
         </div>
